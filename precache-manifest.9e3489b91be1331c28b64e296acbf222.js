b711c7d26b489ac946fe271c7dadde911c5bc16c
Pola Polaczek <pola.polaczek@softwaremind.com>
2020-08-19 16:43:24 +0200

    Updates

---
+++ precache-manifest.9e3489b91be1331c28b64e296acbf222.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb73fe6944f3f654b92a2ac8336d7592",
+    "url": "/typescript-learning-todo-app/index.html"
+  },
+  {
+    "revision": "89caff3a715fd18addc2",
+    "url": "/typescript-learning-todo-app/static/js/2.250e9dcf.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/typescript-learning-todo-app/static/js/2.250e9dcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "408cfde436ebb40893f0",
+    "url": "/typescript-learning-todo-app/static/js/main.25ad9fc0.chunk.js"
+  },
+  {
+    "revision": "2740428acc01694430f5",
+    "url": "/typescript-learning-todo-app/static/js/runtime-main.4e4f0ec5.js"
+  }
+]);
